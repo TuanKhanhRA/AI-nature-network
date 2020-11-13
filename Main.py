365fa3b232e246bf2d80d105423b37176301f79c
TuanKhanhRA <69688469+TuanKhanhRA@users.noreply.github.com>
2020-11-13 08:11:37 +0700

    Updata simple network

---
+++ Main.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+input = np.zeros((1920 ,1200))   # Tạo một numpy array với tất cả phẩn tử là 0
+
+output = np.zeros((1920,1200))   # Tạo một numpy array với tất cả phẩn tử là 0
+print(input)
+print(output)
